cc5cd9e9f0e24f0d9cf0d1cf13a411083088d6a6
eliasenseirb <71278277+eliasenseirb@users.noreply.github.com>
2021-11-08 12:13:29 +0100

    Partie Elias étape 1
    Date.java et l'exception qui ont été crées

---
+++ PG220-master/PG220-master-Etape 1/Etape 1/Commande/Infos.java
@@ -0,0 +1,5 @@
+package Commande
+
+        abstract class Infos {
+
+        }
---
+++ PG220-master/PG220-master-Etape 1/Etape 1/Commande/Main.java
@@ -0,0 +1,19 @@
+package Commande;
+
+    class Main {
+        Date d;
+
+        public static void main(String[] args) {
+            Date d = new Date(12, 03, 15);
+
+            System.out.println("Nous sommes le" + jour + "." + mois + "." + annee);
+        }
+    }
+
+//class Main {
+//    public static void main(String[] args) {
+//        Date d = new Date(12, 03, 15);
+//
+//        System.out.println("Nous sommes le" + jour + "." + mois + "." + annee);
+//    }
+//}
